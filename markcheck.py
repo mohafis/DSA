31fc322be9f1a3ec6cfda198fca7ca0650f21fc6
mohafis <97604131+mohafis@users.noreply.github.com>
2022-04-04 21:35:13 +0800

    Update markcheck.py

--- markcheck.py
+++ markcheck.py
@@ -1,25 +1,38 @@
-y = 50 # minimum passing score
+import streamlit as st
+y = 50
+st.header("Mark Checking Demo")
 
-while True:
-  x = input("Please enter your mark. To stop the algorithm, enter x .\n\n")
-  
-  if x == "x": 
-    print ("\nThank you for using our service.\n")
-    break
-  
-  else:
-    try:
-      val = int(x)
+
+readme = st.checkbox("readme")
+
+if readme:
+
+    st.write("""
+        This is a mark checking demo. You may get the codes via [github](https://github.com/richieyuyongpoh/markchecking)
+        """)
+
+    st.write ("For more info, please contact:")
+
+    st.write("[Dr. Yong Poh Yu](https://www.linkedin.com/in/yong-poh-yu/)")
+    
+
+st.write("Please enter your mark. To stop the algorithm, enter x .\n\n")
+         
+mark = st.text_input('Enter the mark here', '50')
+         
+         
+try:
+    val = float(mark)
             
-      if (val > 100 or val < 0):
-        print("\nPlease enter a valid mark.\n")
+    if (val > 100 or val < 0):
+        st.write("\nPlease enter a valid mark.\n")
                 
-      elif val >= y:
-        print("\nYou passed your exam. Keep it up!\n")
-                
-      else:
-        print("\nUnfortunately, you did not pass your exam. Work harder. You can make it.\n")
+    elif val >= y:
+        st.write("\nYou passed your exam. Keep it up!\n")
+       
+    else:
+        st.write("\nUnfortunately, you did not pass your exam. Work harder. You can make it.\n")
 
             
-    except ValueError:
-      print("\nPlease enter a number.\n")
+except ValueError:
+    st.write("\nPlease enter a number.\n")
